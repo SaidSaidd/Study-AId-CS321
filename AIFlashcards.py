b067f74b4bf198e3f2ea7bda9e2593a5acd9ca8e
Gurkirat Gill <kirat.g@icloud.com>
2025-03-08 11:38:16 -0500

    Added documentation

--- AIFlashcards.py
+++ AIFlashcards.py
@@ -4,6 +4,12 @@ from AIFeatures import AIFeatures
 import re    
 
 class AIFlashcards(AIFeatures):
+    '''
+        Initialize values of AIFlashcard object.
+        Take values from Prexisting AIFeatures object.
+        Change the prompt to match requirements of AIFlashcards.
+        No need to re-upload file.
+    '''
     def __init__(self, aiFeatures):
         # take attributes from already initialized aiFeatures variables.
         self.file_path = aiFeatures.file_path
@@ -20,6 +26,14 @@ class AIFlashcards(AIFeatures):
                       """
         self.uploaded_file = aiFeatures.uploaded_file
         
+    '''
+        Parse the output from Gemini.
+        Store values in a Dictionary.
+        The key of the dictionary is the number of the word.
+        The value is a dictionary itself.
+        The key of the sub dictionary is the word.
+        The value of the sub dictionary is the definition.
+    '''
     def create_dict(self, generated_content):
         self.result_dict = {}
     
@@ -34,8 +48,13 @@ class AIFlashcards(AIFeatures):
         
         return self.result_dict
 
+    '''
+        Get the word from a word and definition pair.
+    '''
     def get_word(self, word_and_def):
         return word_and_def.get("word", "").strip()
-
+    '''
+        Get the definition from a word and definition pair.
+    '''
     def get_def(self, word_and_def):
         return word_and_def.get("definition", "").strip()
--- AISummary.py
+++ AISummary.py
@@ -3,6 +3,12 @@ from google import genai
 from AIFeatures import AIFeatures
 
 class AISummary(AIFeatures):
+    '''
+        Initialize values of AISummary object.
+        Take values from Prexisting AIFeatures object.
+        Change the prompt to match requirements of AISummary.
+        No need to re-upload file.
+    '''
     def __init__(self, aiFeatures):
         # take attributes from already initialized aiFeatures variables.
         self.file_path = aiFeatures.file_path
